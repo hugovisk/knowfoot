5af6c4b9c73eadb6742868b7681b7f4c1ec97db3
Hugo <hugovisk@gmail.com>
2019-09-25 12:10:08 -0300

    doing camera service

--- src/app/services/camera/camera.service.ts
+++ src/app/services/camera/camera.service.ts
@@ -4,18 +4,47 @@ import { DomSanitizer, SafeStyle } from '@angular/platform-browser';
 @Injectable({
   providedIn: 'root'
 })
+
+
+/**
+ * USER CAMERA LIVE VIEW
+ * Author: Hugo Melo
+ * Email: hugo.melo@outlook.com
+ * Version: 0.0.0:20190923
+ *
+ * DESCRIPTION
+ * Access camera and take snap shots through browser with HTML5 video and canvas elements
+ * for Augmented reality on knowfoot hybrid app
+ *
+ * FATURES
+ * - Camera live view [✓]
+ * - Image capture [✓]
+ * - Present preview before salve captured image [✗]
+ * - Access the back camera, fall back to default camera [✗]
+ * - Auto adjust width, height and center of live view [✗]
+ * - Capture image with predefined options TODO: define those option like - rosolution, width, height, quality, mimetype [✗]
+ * - Optimization of image data via capture off lossless pixel data for better post-processing. [✗]
+ * - Use of WebRTC/UserMedia API TODO: implements WebRTC adpter https://github.com/webrtc/adapter [✗]
+ * - TODO: Define better image file format jpg or png smaller size and better quality. [✗]
+ */
+
 export class CameraService {
   private videoElement: HTMLVideoElement;
   cameraTracks: MediaStreamTrack[];
+  // cameraTrack: MediaStreamTrack;
+  constrains: MediaTrackConstraints;
 
   props = {
-    cameraHeight: 120,
-    cameraWidth: 160,
-    captureHeight: 160,
-    captureWidth: 120,
+    cameraHeight: 160,
+    cameraWidth: 120,
+    captureHeight: 130,
+    captureWidth: 130,
     mimeType: 'image/png',
     imageQuality: 1,
-    constraints: null,
+    constraints: {
+      width: { ideal: 260 }, // capture width
+      height: { ideal: 260 } // capture heigth 
+    }
   };
 
   constructor(private sanitizer: DomSanitizer) { }
@@ -28,7 +57,7 @@ export class CameraService {
 
   public startPreview(videoElement: ElementRef) {
     this.cameraPreview = videoElement.nativeElement;
-    this.adjustScale();
+    // this.adjustScale();
     this.getCameraStream();
   }
 
@@ -41,8 +70,8 @@ export class CameraService {
     const quality = 1;
 
     const canvas = document.createElement('canvas');
-    canvas.width = this.cameraWidth;
-    canvas.height = this.cameraHeight;
+    canvas.width = this.props.captureWidth; // this.cameraWidth;
+    canvas.height = this.props.cameraHeight; // this.cameraHeight;
     console.log(this.cameraWidth + ' x ' + this.cameraHeight);
 
     canvas.getContext('2d').
@@ -54,11 +83,35 @@ export class CameraService {
     return this.sanitizer.bypassSecurityTrustStyle(`url(${image})`);
   }
 
+  // public snap() {
+  //   const mimeType = 'image/png';
+  //   const quality = 1;
+
+  //   const canvas = document.createElement('canvas');
+  //   canvas.width = this.props.captureWidth; // this.cameraWidth;
+  //   canvas.height = this.props.cameraHeight; // this.cameraHeight;
+  //   console.log(this.cameraWidth + ' x ' + this.cameraHeight);
+
+  //   // print snapshot as image
+  //   canvas.getContext('2d').drawImage(this.cameraPreview, 0, 0);
+
+  //   // const image = canvas.toDataURL(mimeType, quality);
+  //   // get the ImageData object from the canvas' context.
+  //   const image: ImageData = 
+  //     canvas.getContext('2d').getImageData(0, 0, canvas.width, canvas.height);
+  //   this.stopPreview();
+
+  //   return this.sanitizer.bypassSecurityTrustStyle(`url(${image})`);
+  // }
+
+
+
   private async getCameraStream() {
     try {
       if ('mediaDevices' in navigator && 'getUserMedia' in navigator.mediaDevices) {
-        const stream = await navigator.mediaDevices.getUserMedia({ video: true });
+        const stream = await navigator.mediaDevices.getUserMedia({ video: this.props.constraints });
         this.cameraTracks = stream.getVideoTracks();
+        // this.cameraTrack = stream.getVideoTracks()[0];
         this.displayCameraStream(stream);
       }
     } catch (error) {
@@ -77,5 +130,25 @@ export class CameraService {
     if ((scaleX !== 1.0) || (scaleY !== 1.0)) {
       this.cameraPreview.style.transform = `scaleX(${scaleX}) scaleY(${scaleY})`;
     }
+  }
+
+  cropAndScale() {
+    const props = {
+      image: new Image(),
+      ratio: 0,
+      sw: 0,
+      sh: 0,
+      sx: 0,
+      sy: 0
+    }
+
+    // crop and scale image for final size
+    props.ratio = Math.min(props.image.width / this.props.captureWidth, props.image.height / this.props.captureHeight);
+    props.sw = this.props.captureWidth * props.ratio;
+    props.sh = this.props.captureHeight * props.ratio;
+    props.sx = (props.image.width - props.sw) / 2;
+    props.sy = (props.image.height - props.sh) / 2;
+
+    return props;
   }
 }
